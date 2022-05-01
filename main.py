240fdb778836c8da62de6c08d5a568b091c00c46
Safwaan <hellosafwaan@gmail.com>
2022-05-01 23:37:03 +0530

    new function - cacl_btwn_days()

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import os.path
 from datetime import date
 from shutil import rmtree
 import stat
+
+def calc_btwn_days(d1, d2):
+    date1 = date(d1.year, d1.month, d1.day)
+    date2 = date(d2.year, d2.month, d2.day)
+    return (date2 - date1).days
